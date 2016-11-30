fc1b13cad76caa6b585072db668b08f8b95f38cf
Chris Klug <chris@59north.com>
2016-11-30 11:20:12 +0100

    Removed transpiled files

--- src/AspNetCore-Angular2-Gulp-Starter/ngApp/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-require('./polyfills.ts');
-require('./rxjs.ts');
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-//enableProdMode();
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
